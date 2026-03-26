db1a062a16871872f664be1e9cffeecacc656c1a
Sourabh Gorai <deepugorai100@gmail.com>
2026-03-26 13:31:44 +0530

    added tnp to stack

---
+++ Backend/tnp/src/main/java/com/clubHouse/tnp/TnpApplication.java
@@ -0,0 +1,13 @@
+package com.clubHouse.tnp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TnpApplication.class, args);
+	}
+
+}
---
+++ Backend/tnp/src/test/java/com/clubHouse/tnp/TnpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clubHouse.tnp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TnpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
